535aff8fb02754bd45118790eee7b7fce4c5f6c7
RockTharsh <52143660+RockTharsh@users.noreply.github.com>
2021-09-09 22:00:38 -0400

    Added some basic figures and the meothods drawhands(), chopTwice().

--- main.py
+++ main.py
@@ -1,16 +1,30 @@
 import pygame
 import random
-
+def drawhands(x, y):
+    #screen.blit(handsOriginalResolution, (x, y))
+    #screen.blit(black, (x, y))
+    #screen.blit(paper, (x, y))
+    screen.blit(sissors,(x,y))
+def chopTwice(surface):
+    newSurface = pygame.transform.chop(surface,(0,0,100,0))
+    newSurface = pygame.transform.chop(newSurface,(130,0,450,0))
+    return newSurface
 
 
 
 running = True
 pygame.init()
 screen = pygame.display.set_mode((800, 600))
-hands = pygame.image.load('Draws/Pager rock and sissors .png')
+handsOriginalResolution = pygame.image.load('Draws/tiny .png')
+#tinyHands = pygame.transform.scale(handsOriginalResolution,(380, 161))
+#(x,y,w,h) represents a rectangle with top left point at (x,y), with width w and height h.
+test = pygame.image.load('Draws/SampleTest.png')
+#rock = pygame.transform.chop(handsOriginalResolution, (20, 20, 20, 20))
+sissors = chopTwice(handsOriginalResolution)
+#paper = pygame.transform.chop(handsOriginalResolution,(0,0,220,0))
+#black = pygame.transform.chop(test,(0,0,250,0))
 
-def drawhands(x, y):
-    screen.blit(hands, (x, y))
+
 
 
 
@@ -18,7 +32,8 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    drawhands(0, 0)
+    screen.fill((10, 200, 3))
+    drawhands(200, 450)
     pygame.display.update()
 
 
